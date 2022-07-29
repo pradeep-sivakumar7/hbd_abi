dcf4fd42713b8a4e86138ab1b81109549b997524
pradeep-sivakumar7 <109807399+pradeep-sivakumar7@users.noreply.github.com>
2022-07-29 12:42:38 +0530

    Add files via upload

---
+++ dashboard.js
@@ -0,0 +1,199 @@
+$(window).on('load', function () {
+    $("#loader").fadeOut(1000);
+    $("#content").fadeIn(1000);
+});
+
+//music player
+const musicContainer = document.getElementById('music-container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
+const cover = document.getElementById('cover');
+
+
+
+// Song titles
+
+const songs = ['Bae',
+				'Dhimu Dhimu',
+				'En-Jeevan',
+				'Ennadi-Maayavi-Nee',
+				'Idhazhin-oram',
+				'Innum-Konjam_Neram',
+				'Kannazhaga',
+				'Maalai-Mangum-Neram',
+				'Manamaganin-Sathiyam',
+				'Marakkavillayae',
+				'Mehabooba',
+				'Mellinamae-Mellinamae',
+				'Munbe-Va',
+				'Naalu-Nimisam',
+				'Naan-Nee',
+				'Naan-Un',
+				'Neeyum-Naanum-Anbe',
+				'Nenjukkul-Peithidum',
+				'NewYork-Nagaram',
+				'Oru-Devadhai',
+				'Pachchai-Nirame',
+				'Partha-Muthal',
+				'Pirai-Thedum',
+				'Pookal-Pookum',
+				'Snehidhane',
+				'Suttum-Vizhi',
+				'Thaarame-Tharame',
+				'Thalli-Pogathey',
+				'Un-Vizhigalil',
+				'Uyire-Un-Uyirena',
+				'Vaseegara',
+				'Yaanji'
+]; 
+console.log(songs.length);
+
+const artists = ['IanD.', 
+				'Fairytale (feat. Amanda Collis)', 
+				'Alibi (ft. Heleen)', 
+				'Noah North', 
+				'Colorblind (ft. Halvorsen) ', 
+				' Find You ', 
+				'Xavier Weeks'];
+
+let songIndex = 0;
+
+loadSong(songs[songIndex]);
+
+function loadSong(song){
+	title.innerText = song.replaceAll("-"," ");;
+	artist.innerText = artists[songIndex];
+	audio.src = `music/${song}.mp3`;
+	cover.src = `images/${song}.jpg`;
+}
+
+// Play
+function playSong(){
+	musicContainer.classList.add('play');
+
+	playBtn.querySelector('i.fas').classList.remove('fa-play');
+	playBtn.querySelector('i.fas').classList.add('fa-pause');
+
+	audio.play();
+}
+
+
+// Pause
+function pauseSong(){
+	musicContainer.classList.remove('play');
+
+	playBtn.querySelector('i.fas').classList.add('fa-play');
+	playBtn.querySelector('i.fas').classList.remove('fa-pause');
+
+	audio.pause();
+}
+
+// Previous
+
+function prevSong(){
+	songIndex--;
+
+	if (songIndex < 0) {
+		songIndex = songs.length - 1;
+	}
+
+	loadSong(songs[songIndex]);
+
+	playSong();
+}
+
+// Next
+
+function nextSong(){
+	songIndex++;
+
+	if (songIndex > songs.length - 1) {
+		songIndex = 0;
+	}
+
+	loadSong(songs[songIndex]);
+
+	playSong();
+}
+
+
+// Update progress bar
+function updateProgress(e){
+	const {duration, currentTime} = e.srcElement;
+	const progressPercent = (currentTime/ duration) *100;
+	progress.style.width = `${progressPercent}%`;
+	
+}
+
+
+// Set Progress 
+function setProgress(e) {
+	const width = this.clientWidth;
+	const clickX = e.offsetX;
+	const duration = audio.duration;
+  
+	audio.currentTime = (clickX / width) * duration;
+  }
+
+let currentIndex = songIndex;
+
+function clicked(clicked_id){
+	songIndex = clicked_id;
+	loadSong(songs[songIndex]);
+
+	const isPlaying = musicContainer.classList.contains('play');
+
+	if ((clicked_id == currentIndex) && isPlaying){
+		pauseSong();
+	} else {
+		playSong();
+	}
+
+	currentIndex = clicked_id
+}
+
+
+
+// Event Listener
+
+playBtn.addEventListener('click', () =>{
+	const isPlaying = musicContainer.classList.contains('play');
+
+	if (isPlaying){
+		pauseSong();
+	} else {
+		playSong();
+	}
+})
+
+// Change song
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+
+//Time update
+audio.addEventListener('timeupdate', updateProgress);
+
+
+// Set progress
+progressContainer.addEventListener('click', setProgress);
+
+// Song ended
+audio.addEventListener('ended', nextSong);
+
+
+function rest() {
+	document.getElementById("home").classList.remove("active");
+	document.getElementById("music").classList.add("active");
+}
+
+function rest1(){
+	document.getElementById("home").classList.add("active");
+	document.getElementById("music").classList.remove("active");
+}
---
+++ index.js
@@ -0,0 +1,97 @@
+
+
+
+setInterval(() => {
+    let w1 = document.getElementById("user").value;
+    let w2 = document.getElementById("pin").value;
+
+    if (w1.length > 0 && w2.toString().length == 4) {
+        document.getElementById("next").classList.remove("disabled");
+    }
+    
+    if (w1.length == 0 || w2.toString().length<4) {
+        document.getElementById("next").classList.add("disabled");
+    }
+}, 1);
+
+
+
+
+
+function doIt() {
+    let user = document.getElementById("user").value.toLowerCase().replaceAll(" ", "");
+    let pin = document.getElementById("pin").value;
+    let ch = user + pin;
+
+    if (ch == 'papi2308') {
+        user = "";
+        pin = "";
+        document.getElementById("next").style.color = "white";
+        // document.getElementById("next").style.backgroundColor = "#28A745";
+
+        document.getElementById("next").textContent = "getting you in...";
+        document.getElementById("next").setAttribute('href', 'dashboard.html');
+
+    }
+    else {
+        let add = document.getElementById("addText");
+        let add1 = document.getElementById("addText1");
+
+
+
+        if (user != 'papi') {
+            document.getElementById("user").value = "";
+            document.getElementById("next").classList.add("disabled");
+            document.getElementById("rem1").classList.remove("mb-3");
+            add1.textContent = `Username is not valid!`;
+            let time = setTimeout(() => {
+                add1.textContent = '';
+                document.getElementById("rem1").classList.add("mb-3");
+
+            }, 2000);
+        }
+
+        if (pin != 2308) {
+            document.getElementById("pin").value = "";
+            document.getElementById("next").classList.add("disabled");
+            document.getElementById("rem").classList.remove("mb-3");
+            add.textContent = `Pin is not valid!`;
+            let time = setTimeout(() => {
+                add.textContent = '';
+                document.getElementById("rem").classList.add("mb-3");
+            }, 2000)
+        }
+
+
+
+
+
+    }
+}
+
+let view = false;
+
+function hide() {
+    if (!view) {
+        document.getElementById("basic").innerHTML = "<i class='fa-solid fa-eye-slash'></i>";
+        document.getElementById("pin").setAttribute('type', 'text');
+        document.getElementById("pin").setAttribute('inputmode', 'numeric');
+        view = true;
+    }
+
+    else {
+        document.getElementById("basic").innerHTML = "<i class='fa-solid fa-eye'></i>";
+
+        document.getElementById("pin").setAttribute('type', 'password');
+        document.getElementById("pin").setAttribute('inputmode', 'numeric');
+        view = false;
+    }
+}
+
+
+
+
+$(window).on('load', function () {
+    $("#loader").fadeOut(1000);
+    $("#content").fadeIn(1000);
+});
